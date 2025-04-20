7031209447dcfcaba57bc8055276497c280fb3e8
Love Ahnstrom <love.ahnstrom@gmail.com>
2025-04-20 12:04:10 +0200

    Allow serving of static resources through koa

---
+++ DEPLOY.md
@@ -0,0 +1,3 @@
+## How to deploy the detection-tool
+
+The *detection tool* is a node.js program that fetches clinical trial registration metadata, searches for related publications, and uses ChatGPT to determine if a clinical trial has reported results
--- prototype/index.js
+++ prototype/index.js
@@ -1,182 +1,118 @@
-import fs from "fs";
-import csv from "csvtojson";
-import converter from "json-2-csv";
-import _ from "lodash";
-import seedrandom from "seedrandom";
-import {
-  fetchRegistrationFs,
-  checkSummaryResults,
-  parseRegistration,
-} from "./src/ctgov_utils.js";
-import {
-  discoverPublications,
-  PUB_SOURCE,
-} from "./src/publication_discovery.js";
-import {
-  fetchAbstracts,
-  fetchPubmedRefs,
-  maxDateFilter,
-  mergePubsAbstracts,
-  minDateFilter,
-  parsePubmedAbstractXML,
-} from "./src/pubmed_utils.js";
-import {
-  fetchWithRetries,
-  log,
-  logTimeAndCost,
-  processFinalOutput,
-  removeDuplicatePublications,
-} from "./src/utils.js";
-import { detectResults } from "./src/results_detection.js";
-import { cacheResultToFile } from "./src/cache.js";
-import { fetchArticlesByPmids } from "./src/sqlite_repo.js";
+import Koa from "koa";
+import Router from "koa-router";
+import cors from "@koa/cors";
+import { registrationDiscovery } from "./src/live_processing/registration_discovery.js";
+import { publicationDiscovery } from "./src/live_processing/publication_discovery.js";
+import { resultsDiscovery } from "./src/live_processing/results_discovery.js";
+import serve from "koa-static";
+import path from "path";
+import send from "koa-send";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // To avoid rate limit on chatgpt
 const LIMIT_PUBS = 15;
 
-(async () => {
-  const t1 = Date.now() / 1000;
+const app = new Koa();
+const router = new Router();
 
-  // Dirty solution to keep track of costs
-  global.pubmedTokens = 0;
+// Enable CORS for all origins
+app.use(cors());
 
-  const validationData = await csv().fromFile("./data/validation_dataset.csv");
-
-  const negativeCases = validationData.filter((trial) => {
-    return trial.has_publication === "No" && trial.has_summary_results === "No";
-  });
-
-  const positiveCases = validationData.filter((trial) => {
-    return (
-      trial.has_publication === "Yes" || trial.has_summary_results === "Yes"
-    );
-  });
-
-  // const finalSample = await csv({ delimiter: ";" }).fromFile(
-  //   "./data/final-sample-ctgov.csv"
-  // );
-
-  // For testing purposes:
-  // let output = JSON.parse(
-  //   fs.readFileSync("./out/val/validation_run_error.json")
-  // );
-
-  let output = [];
-
-  // Set seed
-  seedrandom("my_random_seed", { global: true });
-  const _s = _.runInContext();
-
-  // SAMPLE N, this determines what it is going to cost to run!
-  const validationCases = _s.sampleSize(validationData, 100);
-
-  // const cases = validationCases.filter(
-  //   (trial) => trial.has_summary_results === "Yes"
-  // );
-
-  // const sampleCases = _s.sampleSize(finalSample, 10);
-
-  // // SELECT A SPECIFIC CASE
-  // const cases = [
-  //   validationData.find((trial) => trial.nct_id === "NCT01106365"),
-  // ];
-
-  // TODO: FIND OUT WHY THE FOLLOWING NCTID:s FAIL:
-  // NCT02659371, NCT02295696, NCT00860730
-
-  const cases = validationCases;
-
-  for (let trial of cases) {
-    let registration = {};
-    let hasSummaryResults = null;
-    try {
-      // const rawRegistration = await fetchRegistration(trial.nct_id);
-      const rawRegistration = await fetchRegistrationFs(trial.nct_id);
-
-      registration = parseRegistration(rawRegistration);
-      hasSummaryResults = checkSummaryResults(registration);
-
-      // Dirty solution that uses publication pmids from the validation data
-      if (trial.publication_pmid) {
-        registration.publicationPmid = trial?.publication_pmid;
-      }
-
-      const discoveredPublications = await discoverPublications(registration);
-      
-      const uniquePublications = removeDuplicatePublications(
-        discoveredPublications
-      );
-      const truncatedPublications = uniquePublications.slice(0, LIMIT_PUBS);
-      const discoveredPmids = truncatedPublications.map((pub) => pub.pmid);
-      const pubsWithAbstracts = fetchArticlesByPmids(discoveredPmids);
-
-      // This line should be used for all trials
-      const minDayFilteredPublications = minDateFilter(
-        pubsWithAbstracts,
-        registration.startDate
-      );
-
-      // This line should only be used during VALIDATION
-      const filteredPublications = maxDateFilter(
-        minDayFilteredPublications,
-        trial?.registry == "iv" ? "2020-11-17" : "2023-02-15"
-      );
-
-      // For testing purposes:
-      // const publicationResults = pubsWithAbstracts;
-
-      const publicationResults = await cacheResultToFile(
-        () => detectResults(filteredPublications),
-        `results-${trial.nct_id}`
-      );
-
-      output.push({
-        trial,
-        hasSummaryResults,
-        registration,
-        results: publicationResults,
-        error: null,
-        truncated: uniquePublications.length > truncatedPublications.length,
-      });
-    } catch (error) {
-      log(error, true);
-
-      output.push({
-        trial,
-        registration,
-        hasSummaryResults,
-        results: [],
-        error,
-        truncated: false,
-      });
-    }
-  }
-
+// Endpoint to fetch trial data by nctId
+router.get("/api/trials/:nctId", async (ctx) => {
   try {
-    const finalOutput = processFinalOutput(output);
-    const csvOutput = converter.json2csv(finalOutput);
+    const { nctId } = ctx.params;
 
-    fs.writeFileSync(
-      `./out/val/validation_run_${new Date()
-        .toISOString()
-        .replaceAll(".", ":")}.csv`,
-      csvOutput
-    );
-    const t2 = Date.now() / 1000;
-    const totalTokens =
-      finalOutput.reduce((acc, res) => acc + res.toolTokens, 0) +
-      global.pubmedTokens;
-    logTimeAndCost({
-      t1,
-      t2,
-      tokens: totalTokens,
-      nCases: cases.length,
-    });
+    if (!nctId) {
+      ctx.status = 400;
+      ctx.body = { error: "Trial ID is required" };
+      return;
+    }
+
+    const trial = await registrationDiscovery(nctId);
+
+    if (trial) {
+      ctx.body = trial;
+    } else {
+      ctx.status = 404;
+      ctx.body = { error: "Trial not found" };
+    }
   } catch (error) {
-    fs.writeFileSync(
-      "./out/val/validation_run_error.json",
-      JSON.stringify(output)
-    );
-    log(error);
+    ctx.status = 500;
+    ctx.body = { error: "Failed to read trial data" };
   }
-})();
+});
+
+router.get("/api/publications/:nctId", async (ctx) => {
+  try {
+    const { nctId } = ctx.params;
+
+    if (!nctId) {
+      ctx.status = 400;
+      ctx.body = { error: "Trial ID is required" };
+      return;
+    }
+
+    const [pubs, errors] = await publicationDiscovery(nctId);
+
+    if (pubs) {
+      ctx.body = { pubs, errors };
+    } else {
+      ctx.status = 404;
+      ctx.body = { error: "Publications not found" };
+    }
+  } catch (error) {
+    console.error("Error retrieving trial publications:", error);
+    ctx.status = 500;
+    ctx.body = {
+      error: "Failed to retrieve publications for trial ID: " + nctId,
+    };
+  }
+});
+
+router.get("/api/results/:nctId/:pmid", async (ctx) => {
+  const { nctId, pmid } = ctx.params;
+  try {
+    if (!nctId) {
+      ctx.status = 400;
+      ctx.body = { error: "Trial ID is required" };
+      return;
+    }
+
+    if (!pmid) {
+      ctx.status = 400;
+      ctx.body = { error: "PMID is required" };
+      return;
+    }
+
+    const discoveredResults = await resultsDiscovery(nctId, pmid);
+
+    ctx.body = discoveredResults;
+  } catch (error) {
+    console.error("Error searching for results publications:", error);
+    ctx.status = 500;
+    ctx.body = {
+      error: `Failed to retrieve results for publication: ${pmid} and trial ${nctId}`,
+    };
+  }
+});
+
+// Use the router middleware
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// Serve static files from the build folder
+app.use(serve(path.join(__dirname, "build")));
+
+// Fallback for React Router
+app.use(async (ctx) => {
+  await send(ctx, "index.html", { root: path.join(__dirname, "build") });
+});
+// Start the Koa server
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
